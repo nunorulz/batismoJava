45a2854e5474268a463b1bf6868c637addc07841
nunorulz <nunorulz@gmail.com>
2025-05-23 00:33:47 -0300

    Criação da entidade a partir da classe Ninja

---
+++ src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.projeto.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//JPA - Java Percistence API
+// Entity - Transforma uma classe em uma entindade de banco de dados
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
