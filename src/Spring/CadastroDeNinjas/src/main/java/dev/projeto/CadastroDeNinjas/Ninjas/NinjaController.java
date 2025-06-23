8868061aaea4f06c67c3809ead15f618655235cf
nunorulz <nunorulz@gmail.com>
2025-06-23 01:12:37 -0300

    feat: Adiciona o metodo PUT parcialmente, ainda precisa aprimorar o metodo

--- src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -36,9 +36,9 @@ public class NinjaController {
     }
 
     //Alterar dados dos Ninjas (Update)
-    @PutMapping("/alterarID")
-    public String alterarNinjaPorId(){
-        return "Alterar ninja por ID";
+    @PutMapping("/alterar/{id}")
+    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     //Deletar Ninja (Delete)
--- src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -33,4 +33,13 @@ public class NinjaService {
     public void deletarNinjaPorId(Long id){
         ninjaRepository.deleteById(id);
     }
+
+    //Atualizar um ninja
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado){
+        if(ninjaRepository.existsById(id)){
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 }
