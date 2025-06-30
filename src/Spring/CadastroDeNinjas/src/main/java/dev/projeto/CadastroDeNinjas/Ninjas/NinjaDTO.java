dc0e67ead2f08084489769b00dacfa20957a7d41
nunorulz <nunorulz@gmail.com>
2025-06-30 01:26:45 -0300

    Chore: Inclue a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos EntidadeXDTO

--- src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.projeto.CadastroDeNinjas.Ninjas;
 
+import dev.projeto.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String img_url;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
 }
--- src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/Spring/CadastroDeNinjas/src/main/java/dev/projeto/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,34 @@
 package dev.projeto.CadastroDeNinjas.Ninjas;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImg_url(ninjaDTO.getImg_url());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImg_url(ninjaModel.getImg_url());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
